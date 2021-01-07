58d15fde2adfe8ce5380746cc7362be5f5cfdc5d
luan <aluncard6@gmail.com>
2021-01-06 21:31:51 -0300

    criando cordenadas de forma dinamica

--- js/jogo.js
+++ js/jogo.js
@@ -6,6 +6,14 @@ function ajustaTamanhoTela(){
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura,largura)
+    console.log(largura,altura)
 }
      ajustaTamanhoTela()
+
+     // cria cordenadas de forma dinamica
+     var posicaoX = Math.floor(Math.random() * largura)
+     var posicaoY = Math.floor(Math.random() * altura)
+
+     console.log(posicaoX,posicaoY)
+
+     
